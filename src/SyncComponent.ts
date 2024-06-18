b9a2dd50a3ccd7d32ec11764076920e4d7aed948
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-18 06:28:34 +0300

    wip

--- src/SyncComponent.ts
+++ src/SyncComponent.ts
@@ -1,10 +1,14 @@
-import { Modding } from "@flamework/core";
+import { Modding, Reflect } from "@flamework/core";
 
-export const SyncComponent = Modding.createDecorator("Property", (descriptor) => {
-	assert(descriptor.isStatic, "Only static components can be synced!");
-});
+export const SyncComponentKey = "MatterSyncComponent"
 
 /**
- * @metadata flamework:implements flamework:parameters injectable
+ * @metadata flamework:implements flamework:parameters
  */
+export const SyncComponent = Modding.createDecorator("Property", (descriptor) => {
+	assert(descriptor.isStatic, "Only static components can be synced!");
+
+    Reflect.defineMetadata(descriptor.object, SyncComponentKey, true, descriptor.property)
+});
+
 export type SyncComponent = typeof SyncComponent;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export type * from "./Types";
-export * from "./SyncComponent";
+export { SyncComponent } from "./SyncComponent";
 export * from "./ServerSyncer";
 export * from "./ClientSyncer";
