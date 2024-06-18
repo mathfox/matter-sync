609482dfde8c90dda4d91425757a144e2eebda53
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-18 05:52:15 +0300

    throw error for non static components

--- src/SyncComponent.ts
+++ src/SyncComponent.ts
@@ -1,5 +1,7 @@
 import { Modding } from "@flamework/core";
 
-export const SyncComponent = Modding.createMetaDecorator("Property");
+export const SyncComponent = Modding.createDecorator("Property", (descriptor) => {
+    assert(descriptor.isStatic, "Only static components can be synced!")
+});
 
 export type SyncComponent = typeof SyncComponent
