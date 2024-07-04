3947d6a979867d4cba211c7657144d64f64e9dd3
nonamie <53796817+mathfox@users.noreply.github.com>
2024-07-04 14:50:52 +0300

    wip

--- src/__tests__/Components.ts
+++ src/__tests__/Components.ts
@@ -1,5 +1,7 @@
 import { component } from "@rbxts/matter";
 import { addSyncComponent } from "../addSyncComponent";
 
-export const Component = component("Component");
+export const Component = component<{
+	someData: string;
+}>("Component");
 addSyncComponent(Component);
