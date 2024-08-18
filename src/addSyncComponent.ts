2f9f311e862130d25537a17964992c9d16c76fa2
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-18 03:02:08 +0300

    update deps

--- src/addSyncComponent.ts
+++ src/addSyncComponent.ts
@@ -1,6 +1,6 @@
 import type { ComponentCtor } from "@rbxts/matter/lib/component";
 import { componentNameCtorMap } from "./componentNameCtorMap";
 
-export function addSyncComponent(componentCtor: ComponentCtor) {
+export function addSyncComponent(componentCtor: ComponentCtor): void {
 	componentNameCtorMap.set(tostring(componentCtor), componentCtor);
 }
